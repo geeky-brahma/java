ded6d3d1fef0bbc13b31785907586af353b46f00
geeky-brahma <sahoobrahmananda2004.com>
2024-02-03 15:12:39 +0530

    ArraySorter

---
+++ l2/ArraySorter.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArraySorter {
+    public static void main(String[] args) {
+        /
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        
+        int[] array = new int[size];
+
+        
+        System.out.println("Enter the array elements:");
+
+        for (int i = 0; i < size; i++) {
+            System.out.print("Element " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
+        }
+
+        
+        Arrays.sort(array);
+
+        
+        System.out.println("Sorted Array: " + Arrays.toString(array));
+
+        
+    }
+}
